a6b50caf3592b5df239c355a84f3ffd1020c7321
Gautam Panda <gautampanda291@gmail.com>
2026-03-16 15:06:06 +0530

    Updated Django models for Dept, Emp, and Salgrade

--- app/models.py
+++ app/models.py
@@ -6,19 +6,23 @@ class Dept(models.Model):
     dname = models.CharField(max_length=30, unique=True)
     loc = models.CharField(max_length=20)
 
+    
+
 class Emp(models.Model):
     empno = models.IntegerField(primary_key=True)
     ename = models.CharField(max_length=20)
     job = models.CharField(max_length=20)
-    mgr = models.IntegerField(null=True,blank=True)
+    mgr = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
     hiredate = models.DateField()
-    sal = models.DecimalField(max_digits=10,decimal_places=2)
-    comm = models.DecimalField(null=True, blank=True,max_digits=8,decimal_places=2)
+    sal = models.DecimalField(max_digits=10, decimal_places=2)
+    comm = models.DecimalField(null=True, blank=True, max_digits=8, decimal_places=2)
     dept = models.ForeignKey(Dept, on_delete=models.CASCADE)
 
+   
+
 class Salgrade(models.Model):
-    grade=models.IntegerField(primary_key=True)
-    lowsal=models.DecimalField(null=False,max_digits=8,decimal_places=3)
-    hisal=models.DecimalField(null=False,max_digits=6,decimal_places=2)
-    
-    
+    grade = models.IntegerField(primary_key=True)
+    lowsal = models.DecimalField(max_digits=8, decimal_places=2)
+    hisal = models.DecimalField(max_digits=8, decimal_places=2)
+
+ 
